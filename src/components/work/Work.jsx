4a28e31cc5c15d6ebc59be1e4e8cfcb5fc9e06c2
Jesus Ronquillo <bionelectronic@gmail.com>
2023-01-15 13:44:08 -0500

    portafolio

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -4,7 +4,7 @@ import Works from "./Works";
 
 const Work = () => {
   return (
-    <section className="work section">
+    <section id="portfolio" className="work section">
       <h2 className="section__title">Portfolio</h2>
       <span className="section__subtitle">Most recent works</span>
       <Works />
